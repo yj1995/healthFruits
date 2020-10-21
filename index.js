7eb8c898771a0dc77a8403b6be705468342e3823
yj1995 <33519621+yj1995@users.noreply.github.com>
2020-10-21 19:16:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
     app.disable("x-powered-by")
-    app.use(express.static(path.resolve(__dirname, './dist')));
+    app.use(express.static(path.resolve(__dirname, './build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname, './build', 'index.html'));
     })
 }
 const port = process.env.PORT || 8000;
